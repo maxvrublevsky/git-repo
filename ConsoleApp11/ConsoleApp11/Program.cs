b8b5a0477c0e7832e67e7f720ef8fc9024414f21
maxvrulbevsky <maxvrublevsky@yandex.ru>
2019-02-15 13:02:42 +0600

    консольное приложение

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                string s = "";
+                string[] textMass;
+                StreamReader sr = new StreamReader("C:\\test.txt");
+
+                while (sr.EndOfStream != true)
+                {
+                    s += sr.ReadLine();
+                }
+                textMass = s.Split(' ');
+                Console.WriteLine("Количество слов:");
+                Console.WriteLine(textMass.Length);
+
+                sr.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
